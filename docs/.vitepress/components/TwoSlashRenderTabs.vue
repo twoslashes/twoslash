7998c4465ac736ca63736a72b3fe0cf5dde02e96
Anthony Fu <anthonyfu117@hotmail.com>
2024-01-15 15:53:58 +0100

    docs: improve mobile view

--- docs/.vitepress/components/TwoSlashRenderTabs.vue
+++ docs/.vitepress/components/TwoSlashRenderTabs.vue
@@ -5,10 +5,10 @@ const tab = ref(0)
 </script>
 
 <template>
-  <div class="twoslash-render-tabs" mt-6 mb-10>
+  <div class="twoslash-render-tabs">
     <div class="tabs" flex="~ gap-2" text-sm>
       <button
-        flex="~ gap-1 items-center" p1
+        flex="~ gap-1 items-center" px2 py1
         border="b-solid 2 transparent" op50
         :class="{ active: tab === 0 }" @click="tab = 0"
       >
@@ -17,7 +17,7 @@ const tab = ref(0)
       </button>
 
       <button
-        flex="~ gap-1 items-center" p1
+        flex="~ gap-1 items-center" px2 py1
         border="b-solid 2 transparent" op50
         :class="{ active: tab === 1 }" @click="tab = 1"
       >
@@ -37,6 +37,16 @@ const tab = ref(0)
 </template>
 
 <style>
+.twoslash-render-tabs {
+  margin: 16px -24px 32px -24px;
+}
+
+@media (min-width: 640px) {
+  .twoslash-render-tabs {
+    margin: 16px 0 32px 0;
+  }
+}
+
 .twoslash-render-tabs .blocks .inactive {
   display: none;
 }
