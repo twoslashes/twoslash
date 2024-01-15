73d7fffb53d383400396d35e9b542a93d28f4e4c
Anthony Fu <anthonyfu117@hotmail.com>
2024-01-15 13:20:20 +0100

    docs: improve docs looks and feel (#8)

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,15 +1,15 @@
 // https://vitepress.dev/guide/custom-theme
 import Theme from 'vitepress/theme'
 import TwoSlashFloatingVue from 'vitepress-plugin-twoslash/client'
+import type { EnhanceAppContext } from 'vitepress'
+
 import 'vitepress-plugin-twoslash/style.css'
 import 'uno.css'
-import './style.css'
-import './homepage.scss'
-import type { EnhanceAppContext } from 'vitepress'
+import './styles/index.scss'
 
 export default {
   extends: Theme,
   enhanceApp({ app }: EnhanceAppContext) {
-    app.use(TwoSlashFloatingVue)
+    app.use(TwoSlashFloatingVue as any)
   },
 }
