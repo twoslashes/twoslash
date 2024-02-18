f99ab2ab3c21f00394cb57eae376fe4a34bd2db5
Anthony Fu <anthonyfu117@hotmail.com>
2024-02-19 00:07:36 +0100

    test: add more fixtures

---
+++ packages/twoslash-vue/test/fixtures/cut-in-vue.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+// ---cut-start---
+import { ref } from "vue"
+interface Foo {
+  bar: string
+}
+// ---cut-end---
+const { hi } = useFoo()
+// ---cut-start---
+function useFoo() {
+  const hi = ref<Foo>({ bar: "hello" })
+  return { hi }
+}
+declare const foo: {
+  hi: string
+}
+// ---cut-end---
+</script>
+
+<template>
+  <div>
+    {{ hi.bar }}
+    //    ^?
+  </div>
+</template>
