d23618a09ddc6599e35f31e1fc43bce129be65a1
翠 / green <green@sapphi.red>
2024-03-28 17:38:02 +0900

    feat: support spaces before cut comments (#33)

--- packages/twoslash/src/regexp.ts
+++ packages/twoslash/src/regexp.ts
@@ -2,7 +2,7 @@ export const reConfigBoolean = /^\/\/\s?@(\w+)$/mg
 export const reConfigValue = /^\/\/\s?@(\w+):\s?(.+)$/mg
 export const reAnnonateMarkers = /^\s*\/\/\s*\^(\?|\||\^+)( .*)?$/mg
 
-export const reCutBefore = /^\/\/\s?---cut(-before)?---\r?\n/mg
-export const reCutAfter = /^\/\/\s?---cut-after---$/mg
-export const reCutStart = /^\/\/\s?---cut-start---$/mg
-export const reCutEnd = /^\/\/\s?---cut-end---\r?\n/mg
+export const reCutBefore = /^[\t\v\f ]*\/\/\s?---cut(-before)?---\r?\n/mg
+export const reCutAfter = /^[\t\v\f ]*\/\/\s?---cut-after---$/mg
+export const reCutStart = /^[\t\v\f ]*\/\/\s?---cut-start---$/mg
+export const reCutEnd = /^[\t\v\f ]*\/\/\s?---cut-end---\r?\n/mg
