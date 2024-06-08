3ffaf4165370aff66e6a84cb40855b4e579e6213
Matt Pocock <mattpocockvoice@gmail.com>
2024-06-08 19:05:41 +0100

    Fixed import of typescript/lib/tsserver

--- packages/twoslash/test/new.test.ts
+++ packages/twoslash/test/new.test.ts
@@ -1,5 +1,5 @@
 import { expect, it } from 'vitest'
-import * as ts from 'typescript/lib/tsserverlibrary'
+import * as ts from 'typescript/lib/tsserverlibrary.js'
 import { splitFiles } from '../src/utils'
 import type { TwoslashReturn } from '../src/types'
 import { twoslasher } from '../src'
