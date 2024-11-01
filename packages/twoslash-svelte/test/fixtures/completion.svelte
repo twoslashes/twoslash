747b686cd5c4ddd7ffc9c5c7046a774e15ae89b8
hugos68 <hugokorteapple@gmail.com>
2024-11-01 14:52:31 +0100

    Added fixture tests for Svelte

---
+++ packages/twoslash-svelte/test/fixtures/completion.svelte
@@ -0,0 +1,12 @@
+<script>
+    let count = $state(0);
+
+    function increment() {
+        count++;
+    }
+</script>
+
+<button onclick={increment}>
+    Count is: {count}
+//               ^|
+</button>
