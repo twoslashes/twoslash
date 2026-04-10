0d40047a074d755926793e5e125ed01156306696
Anthony Fu <github@antfu.me>
2026-04-10 16:27:38 +0900

    chore: update deps

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,10 +1,9 @@
-import tsconfigPaths from 'vite-tsconfig-paths'
 import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
-  plugins: [
-    tsconfigPaths(),
-  ],
+  resolve: {
+    tsconfigPaths: true,
+  },
   test: {
     benchmark: {
       // reporters: ['default', 'json'],
